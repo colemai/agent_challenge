693e58647f6ff9f755286b067cd2dc39c8cf686d
colemai <colemai@tcd.ie>
2020-05-17 15:14:50 +0100

    http req agent passing basic tests

--- src/main.py
+++ src/main.py
@@ -3,14 +3,17 @@
 """
 Author: Ian
 Purpose: Coding exercise, agent based orchestration
-Input: 
+Input: path to json file
 Output:
-Sample Run:
+Sample Run: python src/main.py data/tiny-tines-sunset.json
 """
 
 from sys import argv
 import json
 import pytest
+# import jmespath
+import requests
+import pdb
 
 
 def ingest_story(path_to_story):
@@ -23,18 +26,29 @@ def process_story(story):
 	""" process a json story through agents the specified agents """
 	assert type(story) is dict
 	assert bool(story) == True # check not empty
+	event = {}
+	for agent in story['agents']:
+		if agent['type'] == 'HTTPRequestAgent':
+			event[agent['name']] = http_req_agent(agent['options'], event)
+			return event
 
 
 
-def http_req_agent (options, output={}):
+def http_req_agent (options, event={}):
 	""" Make a http req to options.url, filling in gaps from output """
 	assert type(options) is dict
 	assert 'url' in options
 	assert isinstance(options['url'], str)
 	assert bool(options) == True # check not empty
+	print(options)
+	response = requests.get(options['url'])
+	assert response.status_code == 200 #TODO account for fails
+	#TODO create log file and add response.headers
 
+	output = json.loads(response.text)
+	return output
 
-def print_agent (options, output={}):
+def print_agent (options, event={}):
 	""" print options.message to terminal """
 	assert type(options) is dict
 	assert 'message' in options
@@ -46,5 +60,5 @@ def print_agent (options, output={}):
 if __name__ == '__main__':
     path_to_story = argv[1]
     story = ingest_story(path_to_story)
-    http_req_agent(story)
-    print(story)
+    event = process_story(story)
+    print(event)
--- src/test_main.py
+++ src/test_main.py
@@ -15,3 +15,8 @@ from src import main
 def test_ingest_story():
 	"""pytest for test case input"""
 	assert main.ingest_story("data/tiny-tines-sunset.json") == {'agents': [{'type': 'HTTPRequestAgent', 'name': 'location', 'options': {'url': 'http://free.ipwhois.io/json/'}}, {'type': 'HTTPRequestAgent', 'name': 'sunset', 'options': {'url': 'https://api.sunrise-sunset.org/json?lat={{location.latitude}}&lng={{location.longitude}}'}}, {'type': 'PrintAgent', 'name': 'print', 'options': {'message': 'Sunset in {{location.city}}, {{location.country}} is at {{sunset.results.sunset}}.'}}]}
+
+def test_http_req_agent():
+	ip_ans = main.http_req_agent({'url': 'http://free.ipwhois.io/json/'})
+	assert type(ip_ans) is dict 
+	assert 'ip' in ip_ans
