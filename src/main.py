9f78ef32ea18d65069c4ed802e7eb431e4d89f2d
colemai <colemai@tcd.ie>
2020-05-17 17:05:08 +0100

    Solved empty var and whitespace var for formatting url/msg

--- src/main.py
+++ src/main.py
@@ -4,19 +4,16 @@
 Author: Ian
 Purpose: Coding exercise, agent based orchestration
 Input: path to json file
-Output:
+Output: 
 Sample Run: python src/main.py data/tiny-tines-sunset.json
 """
 
 from sys import argv
 import json
-import pytest
 import jmespath
 import requests
-import pdb
 import re
-# import logging
-# logging.basicConfig(level=logging.INFO)
+import pdb
 
 
 def ingest_story(path_to_story):
@@ -25,28 +22,32 @@ def ingest_story(path_to_story):
 	  story = json.load(f)
 	return story
 
+
 def process_story(story):
 	""" process a json story through agents the specified agents """
 	assert type(story) is dict
 	assert bool(story) == True # check not empty
 	event = {}
 	for agent in story['agents']:
-		print('Running agent', agent['name'])
+		# print('Running agent', agent['name'])
 		if agent['type'] == 'HTTPRequestAgent':
 			event[agent['name']] = http_req_agent(agent['options'], event)
-		if agent['type'] == 'PrintAgent':
+		elif agent['type'] == 'PrintAgent':
 			event[agent['name']] = print_agent(agent['options'], event)
+		else:
+			sys.exit('Ruh Roh, invalid agent name')
 
 
 def format_agent_arg (event, arg):
-	""" If an arg has double curly brackets, replace the section with the 
+	""" If an arg has double curly brackets, replace that section with the 
 	specified key from event """
+	# arg = 'https://api.sunrise-sunset.or{{ }}g/json?lat={{location.latitude}}&lng={{location.longitude}}'
 	pattern = r"\{\{(.*?)\}\}"
 	matches = re.findall(pattern, arg)
-	values = [jmespath.search(match, event) for match in matches]
+	values = [jmespath.search(match, event) if (bool(match) and not str(match).isspace()) else match for match in matches]
+	pdb.set_trace()
 	new_arg = re.sub(pattern, '{}', arg).format(*values)
 	return new_arg
-
 
 
 def http_req_agent (options, event={}):
@@ -57,16 +58,17 @@ def http_req_agent (options, event={}):
 	assert bool(options) == True # check not empty
 
 	url = options['url']
-	# If the url needs input from previous agent:
+	# Incorporate previous output:
 	if event != {}:
 		url = format_agent_arg(event, url)
 
 	response = requests.get(url)
-	assert response.status_code == 200 #TODO account for fails
+	assert response.status_code in range(200, 300) #TODO account for fails
 	#TODO create log file and add response.headers
 
 	output = json.loads(response.text)
 	return output
+
 
 def print_agent (options, event={}):
 	""" print options.message to terminal """
@@ -75,10 +77,11 @@ def print_agent (options, event={}):
 	assert isinstance(options['message'], str)
 	assert bool(options) == True # check not empty
 
-	print (options, event)
 	message = options['message']
+	# Incorporate previous output:
 	if event != {}:
 		message = format_agent_arg(event, message)
+	
 	print(message)
 	return message
 
@@ -87,4 +90,4 @@ if __name__ == '__main__':
     path_to_story = argv[1]
     story = ingest_story(path_to_story)
     event = process_story(story)
-    print(event)
+    #TODO save event to output file
--- src/test_main.py
+++ src/test_main.py
@@ -3,8 +3,8 @@
 """
 Author: Ian
 Purpose: test the main script
-Input: 
-Output:
+Input: -
+Output: -
 Sample Run: pytest
 """
 
@@ -12,20 +12,52 @@ import pytest
 
 from src import main
 
+
 def test_ingest_story():
 	"""pytest for test case input"""
 	assert main.ingest_story("data/tiny-tines-sunset.json") == {'agents': [{'type': 'HTTPRequestAgent', 'name': 'location', 'options': {'url': 'http://free.ipwhois.io/json/'}}, {'type': 'HTTPRequestAgent', 'name': 'sunset', 'options': {'url': 'https://api.sunrise-sunset.org/json?lat={{location.latitude}}&lng={{location.longitude}}'}}, {'type': 'PrintAgent', 'name': 'print', 'options': {'message': 'Sunset in {{location.city}}, {{location.country}} is at {{sunset.results.sunset}}.'}}]}
+
 
 def test_http_req_agent():
 	ip_ans = main.http_req_agent({'url': 'http://free.ipwhois.io/json/'})
 	assert type(ip_ans) is dict 
 	assert 'ip' in ip_ans
 
+
+def test_http_req_agent_sunset_req ():
 	sunset_ans = main.http_req_agent({'url': 'https://api.sunrise-sunset.org/json?lat={{location.latitude}}&lng={{location.longitude}}'}, {'location': {'ip': '109.78.65.85', 'success': True, 'type': 'IPv4', 'continent': 'Europe', 'continent_code': 'EU', 'country': 'Ireland', 'country_code': 'IE', 'country_flag': 'https://cdn.ipwhois.io/flags/ie.svg', 'country_capital': 'Dublin', 'country_phone': '+353', 'country_neighbours': 'GB', 'region': 'County Dublin', 'city': 'Dublin', 'latitude': '53.3165322', 'longitude': '-6.3425318', 'asn': 'AS15502', 'org': 'Vodafone Ireland', 'isp': 'Vodafone Ireland Limited', 'timezone': 'Europe/Dublin', 'timezone_name': 'Greenwich Mean Time', 'timezone_dstOffset': '0', 'timezone_gmtOffset': '0', 'timezone_gmt': 'GMT 0:00', 'currency': 'Euro', 'currency_code': 'EUR', 'currency_symbol': '€', 'currency_rates': '0.924078', 'currency_plural': 'euros', 'completed_requests': 46}})
-	assert type(ip_ans) is dict
+	assert type(sunset_ans) is dict
 	assert 'results' in sunset_ans
 	assert 'sunrise' in sunset_ans['results']
+
 
 def test_print_agent():
 	ans = main.print_agent({'message': 'Sunset in {{location.city}}, {{location.country}} is at {{sunset.results.sunset}}.'}, {'location': {'ip': '109.78.65.85', 'success': True, 'type': 'IPv4', 'continent': 'Europe', 'continent_code': 'EU', 'country': 'Ireland', 'country_code': 'IE', 'country_flag': 'https://cdn.ipwhois.io/flags/ie.svg', 'country_capital': 'Dublin', 'country_phone': '+353', 'country_neighbours': 'GB', 'region': 'County Dublin', 'city': 'Dublin', 'latitude': '53.3165322', 'longitude': '-6.3425318', 'asn': 'AS15502', 'org': 'Vodafone Ireland', 'isp': 'Vodafone Ireland Limited', 'timezone': 'Europe/Dublin', 'timezone_name': 'Greenwich Mean Time', 'timezone_dstOffset': '0', 'timezone_gmtOffset': '0', 'timezone_gmt': 'GMT 0:00', 'currency': 'Euro', 'currency_code': 'EUR', 'currency_symbol': '€', 'currency_rates': '0.924078', 'currency_plural': 'euros', 'completed_requests': 48}, 'sunset': {'results': {'sunrise': '4:20:36 AM', 'sunset': '8:23:00 PM', 'solar_noon': '12:21:48 PM', 'day_length': '16:02:24', 'civil_twilight_begin': '3:35:22 AM', 'civil_twilight_end': '9:08:14 PM', 'nautical_twilight_begin': '2:29:56 AM', 'nautical_twilight_end': '10:13:40 PM', 'astronomical_twilight_begin': '12:00:01 AM', 'astronomical_twilight_end': '12:00:01 AM'}, 'status': 'OK'}})
 	assert ans == "Sunset in Dublin, Ireland is at 8:23:00 PM."
+
+
+def test_format_agent_arg():
+	event = {'location': {'ip': '109.78.65.85', 'success': True, 'type': 'IPv4', 'continent': 'Europe', 'continent_code': 'EU', 'country': 'Ireland', 'country_code': 'IE', 'country_flag': 'https://cdn.ipwhois.io/flags/ie.svg', 'country_capital': 'Dublin', 'country_phone': '+353', 'country_neighbours': 'GB', 'region': 'County Dublin', 'city': 'Dublin', 'latitude': '53.3165322', 'longitude': '-6.3425318', 'asn': 'AS15502', 'org': 'Vodafone Ireland', 'isp': 'Vodafone Ireland Limited', 'timezone': 'Europe/Dublin', 'timezone_name': 'Greenwich Mean Time', 'timezone_dstOffset': '0', 'timezone_gmtOffset': '0', 'timezone_gmt': 'GMT 0:00', 'currency': 'Euro', 'currency_code': 'EUR', 'currency_symbol': '€', 'currency_rates': '0.924078', 'currency_plural': 'euros', 'completed_requests': 60}}
+	arg = "https://api.sunrise-sunset.org/json?lat={{location.latitude}}&lng={{location.longitude}}"
+	ans = main.format_agent_arg(event, arg)
+	assert ans == "https://api.sunrise-sunset.org/json?lat=53.3165322&lng=-6.3425318"
+
+
+def test_format_agent_arg_unknown_key():
+	event = {'location': {'ip': '109.78.65.85', 'success': True, 'type': 'IPv4', 'continent': 'Europe', 'continent_code': 'EU', 'country': 'Ireland', 'country_code': 'IE', 'country_flag': 'https://cdn.ipwhois.io/flags/ie.svg', 'country_capital': 'Dublin', 'country_phone': '+353', 'country_neighbours': 'GB', 'region': 'County Dublin', 'city': 'Dublin', 'latitude': '53.3165322', 'longitude': '-6.3425318', 'asn': 'AS15502', 'org': 'Vodafone Ireland', 'isp': 'Vodafone Ireland Limited', 'timezone': 'Europe/Dublin', 'timezone_name': 'Greenwich Mean Time', 'timezone_dstOffset': '0', 'timezone_gmtOffset': '0', 'timezone_gmt': 'GMT 0:00', 'currency': 'Euro', 'currency_code': 'EUR', 'currency_symbol': '€', 'currency_rates': '0.924078', 'currency_plural': 'euros', 'completed_requests': 60}}
+	arg = "https://api.sunrise-sunset.or{{location.falsestring8}}g/json?lat={{location.latitude}}&lng={{location.longitude}}"
+	ans = main.format_agent_arg(event, arg)
+	assert ans == "https://api.sunrise-sunset.org/json?lat=53.3165322&lng=-6.3425318"
+
+
+def test_format_agent_arg_empty_key():
+	event = {'location': {'ip': '109.78.65.85', 'success': True, 'type': 'IPv4', 'continent': 'Europe', 'continent_code': 'EU', 'country': 'Ireland', 'country_code': 'IE', 'country_flag': 'https://cdn.ipwhois.io/flags/ie.svg', 'country_capital': 'Dublin', 'country_phone': '+353', 'country_neighbours': 'GB', 'region': 'County Dublin', 'city': 'Dublin', 'latitude': '53.3165322', 'longitude': '-6.3425318', 'asn': 'AS15502', 'org': 'Vodafone Ireland', 'isp': 'Vodafone Ireland Limited', 'timezone': 'Europe/Dublin', 'timezone_name': 'Greenwich Mean Time', 'timezone_dstOffset': '0', 'timezone_gmtOffset': '0', 'timezone_gmt': 'GMT 0:00', 'currency': 'Euro', 'currency_code': 'EUR', 'currency_symbol': '€', 'currency_rates': '0.924078', 'currency_plural': 'euros', 'completed_requests': 60}}
+	arg = "https://api.sunrise-sunset.or{{}}g/json?lat={{location.latitude}}&lng={{location.longitude}}"
+	ans = main.format_agent_arg(event, arg)
+	assert ans == "https://api.sunrise-sunset.org/json?lat=53.3165322&lng=-6.3425318"
+
+def test_format_agent_arg_unmatching_braces():
+	event = {'location': {'ip': '109.78.65.85', 'success': True, 'type': 'IPv4', 'continent': 'Europe', 'continent_code': 'EU', 'country': 'Ireland', 'country_code': 'IE', 'country_flag': 'https://cdn.ipwhois.io/flags/ie.svg', 'country_capital': 'Dublin', 'country_phone': '+353', 'country_neighbours': 'GB', 'region': 'County Dublin', 'city': 'Dublin', 'latitude': '53.3165322', 'longitude': '-6.3425318', 'asn': 'AS15502', 'org': 'Vodafone Ireland', 'isp': 'Vodafone Ireland Limited', 'timezone': 'Europe/Dublin', 'timezone_name': 'Greenwich Mean Time', 'timezone_dstOffset': '0', 'timezone_gmtOffset': '0', 'timezone_gmt': 'GMT 0:00', 'currency': 'Euro', 'currency_code': 'EUR', 'currency_symbol': '€', 'currency_rates': '0.924078', 'currency_plural': 'euros', 'completed_requests': 60}}
+	arg = "https://api.sunrise-sunset.or{{}g/json?lat={{location.latitude}}&lng={{location.longitude}}"
+	ans = main.format_agent_arg(event, arg)
+	assert ans == "https://api.sunrise-sunset.or{{}g/json?lat=53.3165322&lng=-6.3425318"
