2464fa3185f1ba39167de91588aeedcf845d6379
colemai <colemai@tcd.ie>
2020-05-17 12:47:42 +0100

    Ingesting story

---
+++ agent_challenge/main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""
+Author: Ian
+Purpose: Coding exercise, agent based orchestration
+Input: 
+Output:
+Sample Run:
+"""
+
+from sys import argv
+import json
+
+def ingest_story(path_to_story):
+	"""Takes path to story (json), returns dict"""
+	with open(path_to_story) as f:
+	  story = json.load(f)
+	return story
+
+if __name__ == '__main__':
+    path_to_story = argv[1]
+    story = ingest_story(path_to_story)
+    print(story)
